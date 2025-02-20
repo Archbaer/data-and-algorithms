dd1b6034dbcc98d23b91b6428b1ca10ec9960f8b
archbaer <pereirakebaili@gmail.com>
2025-02-20 21:29:17 +0000

    new quickSort

--- QuickSort/src/QuickSortVariant.java
+++ QuickSort/src/QuickSortVariant.java
@@ -1,2 +1,40 @@
+import java.util.Arrays;
+
 public class QuickSortVariant {
+
+    public static void main(String[] args) {
+        int [] arr = {7, 3, 1, 5, 2, 8, 6, 4};
+        quickSort(arr, 0, arr.length -1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j= low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }
